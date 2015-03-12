5022dd870c244c53624f8577a037d93fc684e019
rianarandoja <rianarandoja@gmail.com>
2015-03-13 00:19:38 +0200

    Update Aken.java

--- Aken.java
+++ Aken.java
@@ -1,4 +1,5 @@
 package projekt;
+
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Frame;
@@ -6,113 +7,76 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Aken extends JFrame implements MouseListener{
-JFrame frame;
-File pilt = new File("C:/Users/Getu/Desktop/screenshot.png");
-BufferedImage image = ImageIO.read(pilt);
-BufferedImage img;
-String hiir = "";
+public class Aken extends JFrame implements MouseListener, ActionListener {
+    //kõik erinevad kursori pildid on siin listis
+    ArrayList<String> pilt_list  = new ArrayList<String>(Arrays.asList(
+            "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack.gif",
+            "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\pomm2.gif"));
+
+    JFrame frame;
+    File pilt = new File("C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\screenshot.png");
+    BufferedImage image = ImageIO.read(pilt);
+    BufferedImage img;
+
+    // muutujad kursori pildi vahetuseks
+    String viimatine_pilt;
+    int i = 0;
+    int list_pikkus = pilt_list.size();
 
 
-	void randomImage() throws Exception{
-		int random = (int) Math.round(Math.random()* 3);
-		if(random == 1){
-			this.img = ImageIO.read(new File("C:/Users/Getu/Desktop/crack.gif"));
-		}
-		else if(random == 2){
-			this.img = ImageIO.read(new File("C:/Users/Getu/Desktop/crack2.gif"));
-		}else{
-			this.img = ImageIO.read(new File("C:/Users/Getu/Desktop/crack3.gif"));
-		}
-	}
-	//void mouseClick(MouseEvent e){
-	//	Graphics2D g2d = image.createGraphics();
-	//	g2d.setColor(Color.RED);
-	//	g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
-	//	g2d.dispose();
-	//	Point asukoht = frame.getMousePosition();
-	//	Graphics g = frame.getGraphics();
-	//	g.drawLine(asukoht.x-10, asukoht.y, asukoht.x+10, asukoht.y);
-	//}
-	
-	public void mousePressed(MouseEvent e){
-	    if(e.getButton() == MouseEvent.BUTTON1){
-	    	Point asukoht = frame.getMousePosition();
-	    	Graphics g = frame.getGraphics();
-			//g.drawLine(asukoht.x-10, asukoht.y, asukoht.x+10, asukoht.y);
-	    	try {
-				randomImage();
-			} catch (Exception e1) {
-				
-			}
-			g.drawImage(img, asukoht.x-30, asukoht.y-30, null);
-	    }	    
-	    
-	    else if(e.getButton() == MouseEvent.BUTTON3){//CURSORI PILDI MUUTMINE HETKEL
-	    	Toolkit toolkit = Toolkit.getDefaultToolkit();
-	    	  Image image = toolkit.getImage("C:/Users/Getu/Desktop/crack.gif");
-	    	  Cursor c = toolkit.createCustomCursor(image , new Point(frame.getX(), frame.getY()), "img");
-	    	  frame.setCursor (c);
-	    }
-	    else if(e.getButton() == MouseEvent.BUTTON2) {
-	    	String hiir = "Pomm";
-	    	Graphics g = frame.getGraphics();
-	    	Toolkit toolkit = Toolkit.getDefaultToolkit();
-	    	Image image = toolkit.getImage("C:/Users/Kalvar/Desktop/pomm2.gif");
-	    	Cursor c = toolkit.createCustomCursor(image , new Point(frame.getX(), frame.getY()), "img");
-	    	frame.setCursor (c);
-	}
-}
-	
-		Aken() throws Exception{
+    public void mousePressed(MouseEvent event) {
+
+        //vasak hiireklikk ehk midagi tekib ekraanile juurde
+        if(event.getButton() == MouseEvent.BUTTON1){
+            Point asukoht = frame.getMousePosition();
+            Graphics g = frame.getGraphics();
+            try {
+                if (viimatine_pilt.endsWith("k.gif")) {img = ImageIO.read(new File(Pildid.crack()));}
+                else if (viimatine_pilt.endsWith("2.gif")) {img = ImageIO.read(new File(Pildid.pomm()));}
+            } catch (Exception e1) {}
+            g.drawImage(img, asukoht.x - 30, asukoht.y - 30, null);}
+
+        // vahetab kursori pilti (võtab listist järjest pilte), parem hiireklikk
+        else if(event.getButton() == MouseEvent.BUTTON3){
+            setKursor(pilt_list.get(i));
+            viimatine_pilt = pilt_list.get(i);
+            i++;
+            if (i == list_pikkus){i = 0;}}}
+
+
+    public void setKursor(String pilt) {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Image image = toolkit.getImage(pilt);
+            Cursor c = toolkit.createCustomCursor(image , new Point(frame.getX(), frame.getY()), "img");
+            frame.setCursor (c);}
+
+    Aken() throws Exception{
         JLabel label = new JLabel(new ImageIcon(image));
-		frame = new JFrame("Test");
-		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-		frame.setUndecorated(true);
-		frame.createImage(frame.getWidth(), frame.getHeight());
-		frame.show();
+        frame = new JFrame("Test");
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.createImage(frame.getWidth(), frame.getHeight());
+        frame.show();
         frame.getContentPane().add(label);
         frame.pack();
         frame.setVisible(true);
-		frame.addMouseListener(this);
-		
-		
-   
-	}
-	
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        frame.addMouseListener(this);}
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-}
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void actionPerformed(ActionEvent event) {}}
