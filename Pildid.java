2ba8f67b81b1f68ddd5753f268d09fcc24c9eb81
rianarandoja <rianarandoja@gmail.com>
2015-03-13 00:20:47 +0200

    Create Pildid.java

---
+++ Pildid.java
@@ -0,0 +1,12 @@
+package projekt;
+
+public class Pildid{
+
+    static String crack(){
+        int random = (int) Math.round(Math.random()* 3);
+        if(random == 1){return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack.gif";}
+        else if(random == 2){return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack2.gif";}
+        else{return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack3.gif";}}
+
+    static String pomm() {return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\plahvatus4.gif";}
+}
