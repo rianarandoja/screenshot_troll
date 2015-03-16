15c1d113f81dc96eaa800be18b3840b7db9b2c01
Megahit <kalvarseemen@gmail.com>
2015-03-17 01:28:17 +0200

    Update Pildid.java

--- Pildid.java
+++ Pildid.java
@@ -7,6 +7,16 @@ public class Pildid{
         if(random == 1){return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack.gif";}
         else if(random == 2){return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack2.gif";}
         else{return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\crack3.gif";}}
+        
+    static String värv() {
+    	int random = (int) Math.round(Math.random()* 6);
+    	if(random == 1) {return "C:/Users/Kalvar/Desktop/sinine.gif";}
+    	else if(random == 2) {return "C:/Users/Kalvar/Desktop/roheline.gif";}
+    	else if(random == 3) {return "C:/Users/Kalvar/Desktop/oranž.gif";}
+    	else if(random == 4) {return "C:/Users/Kalvar/Desktop/kollane.gif";}
+    	else if(random == 5) {return "C:/Users/Kalvar/Desktop/hsinine.gif";}
+    	else {return "C:/Users/Kalvar/Desktop/punane.gif";}
+    }
 
     static String pomm() {return "C:\\Users\\Kasutaja\\OneDrive\\TARTU ÜLIKOOL\\OOP\\IntelliJ projects\\src\\projekt\\plahvatus4.gif";}
 }
