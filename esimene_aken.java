f24c6b552342519814d5d5686cfb0a264b165892
rianarandoja <rianarandoja@gmail.com>
2015-03-17 16:45:36 +0200

    Update esimene_aken.java

--- esimene_aken.java
+++ esimene_aken.java
@@ -15,54 +15,39 @@ import java.text.AttributedCharacterIterator;
 
 public class esimene_aken implements ActionListener{
 
-    //public static void main(String[] args) {algus();}
     JFrame frame = new JFrame("Hammer time!");
 
     esimene_aken() {
 
         JPanel mainPanel = new JPanel(new GridBagLayout());
-
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.anchor = GridBagConstraints.PAGE_START;
 
         JLabel ope = new JLabel("Mängu juhised: ", SwingConstants.CENTER);
         mainPanel.add(ope, gbc);
 
+        JLabel üks = new JLabel("           "); mainPanel.add(üks, gbc);
 
+        JLabel eesmärk = new JLabel("Mängu eesmärk on stressi maandamine.", SwingConstants.CENTER);
+        mainPanel.add(eesmärk, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy = 2;
+        JLabel kolm = new JLabel("           "); mainPanel.add(kolm, gbc);
 
-        JLabel parem = new JLabel("Parem hiireklõps - valid vahendi, millega edasi toimida.", SwingConstants.CENTER);
+        JLabel parem = new JLabel("     Et valida vahendit, millega edasi toimida, kliki numbriklahvidel 1 - ....     ", SwingConstants.CENTER);
         mainPanel.add(parem, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy = 4;
+        JLabel kaks = new JLabel("           "); mainPanel.add(kaks, gbc);
 
         JLabel vasak = new JLabel("Vajuta vasakut hiireklahvi, et valitud vahendit rakendada.", SwingConstants.CENTER);
         mainPanel.add(vasak, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        JLabel eesmärk = new JLabel("Mängu eesmärk on stressi maandamine.", SwingConstants.CENTER);
-        mainPanel.add(eesmärk, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-
+        JLabel neli = new JLabel("           "); mainPanel.add(neli, gbc);
 
         JButton start = new JButton("Alusta mängu!");
         mainPanel.add(start, gbc);
-
-        mainPanel.add(Box.createRigidArea(new Dimension(150, 300)));
-
 
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().add(mainPanel);
@@ -70,12 +55,6 @@ public class esimene_aken implements ActionListener{
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
 
-        start.addActionListener(this);
+        start.addActionListener(this);}
 
-    }
-
-    public void actionPerformed(ActionEvent e){
-        frame.dispose();
-
-    }
-}
+    public void actionPerformed(ActionEvent e){frame.dispose();}}
